4b42ce875829604044093f2f85673625934e5060
magfurulabeer <magfurulabeer@gmail.com>
2019-12-13 11:55:59 -0500

    Add Shipment model

--- client-service/src/main/java/com/example/client/clientservice/model/Shipment.java
+++ client-service/src/main/java/com/example/client/clientservice/model/Shipment.java
@@ -1,4 +1,28 @@
 package com.example.client.clientservice.model;
 
 public class Shipment {
+    private Long id;
+
+    private String name;
+
+    private Long trackingNumber;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Long getTrackingNumber() {
+        return trackingNumber;
+    }
+    public void setTrackingNumber(Long trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
 }
